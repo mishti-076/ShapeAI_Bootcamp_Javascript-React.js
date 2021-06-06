02b5c128726ba26848bac24f5f12c734deda3023
mishti-076 <83333009+mishti-076@users.noreply.github.com>
2021-06-06 19:28:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javasript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken by Shauya Sinha. We covered
+        everything from Scratch including Javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
